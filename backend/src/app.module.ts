907bdaf3172ea0513cfe385879e68a0c0bfc8461
Michael <trainsfast@protonmail.com>
2025-04-23 18:32:22 +0300

    fix: changed .env.example to support development flow

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,20 +2,19 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskModule } from './task/task.module';
 import { AppConfigModule } from './config/app-config.module';
-import { ConfigService } from "@nestjs/config";
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-	imports: [
-		AppConfigModule,
-		MongooseModule.forRootAsync({
-			imports: [AppConfigModule],
-			useFactory: (cfg: ConfigService) => ({
-				uri: cfg.get<string>('database.mongodb.uri'),
-			}),
-			inject: [ConfigService],
-		}),
-		TaskModule,
-	]
+  imports: [
+    AppConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [AppConfigModule],
+      useFactory: (cfg: ConfigService) => ({
+        uri: cfg.get<string>('database.mongodb.uri'),
+      }),
+      inject: [ConfigService],
+    }),
+    TaskModule,
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}
